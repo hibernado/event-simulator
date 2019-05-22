e8ec6e318240360d914c190e78cbaaa07a04e69b
hibernado <oboyle.a@gmail.com>
2019-05-22 08:09:34 +0100

    streaming context

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+
+batch_duration_seconds = 5
 
 sc = SparkContext.getOrCreate()
+ssc = StreamingContext(sc, batch_duration_seconds)
